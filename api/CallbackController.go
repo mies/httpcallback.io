22ccecc70a72e4cecdc5759a37e06f5b0ed53202
Pieter Joost van de Sande <pj@born2code.net>
2013-06-22 21:47:18 +0200

    Refactor CallbackController

--- api/CallbackController.go
+++ api/CallbackController.go
@@ -3,6 +3,9 @@ package api
 import (
 	"github.com/pjvds/httpcallback.io/data"
 	"github.com/pjvds/httpcallback.io/model"
+	"labix.org/v2/mgo/bson"
+	"net/http"
+	"time"
 )
 
 type CallbackController struct {
@@ -15,6 +18,29 @@ func NewCallbackController(callbacks data.CallbackRepository) *CallbackControlle
 	}
 }
 
-func (ctr *CallbackController) AddCallback(callback *model.Callback) {
-	ctr.callbacks.Add(callback)
+func (ctr *CallbackController) NewCallback(r *http.Request, args *model.CallbackRequest) (*JsonResponse, error) {
+	callback := model.Callback{
+		Id:        bson.NewObjectId().String(),
+		CreatedAt: time.Now(),
+		Request:   args,
+	}
+	err := ctr.callbacks.Add(&callback)
+	if err != nil {
+		return nil, err
+	}
+
+	return JsonResult(&struct {
+		Id string `json:"id"`
+	}{
+		Id: callback.Id,
+	})
+}
+
+func (ctr *CallbackController) ListCallbacks(r *http.Request) (*JsonResponse, error) {
+	callbacks, err := ctr.callbacks.List()
+	if err != nil {
+		return nil, err
+	} else {
+		return JsonResult(callbacks)
+	}
 }
--- model/CallbackState.go
+++ model/CallbackState.go
@@ -9,3 +9,11 @@ type Callback struct {
 	CreatedAt time.Time        `json:"createAt"`
 	Request   *CallbackRequest `json:"request"`
 }
+
+func NewCallback(id string, request *CallbackRequest) *Callback {
+	return &Callback{
+		Id:        id,
+		CreatedAt: time.Now(),
+		Request:   request,
+	}
+}
