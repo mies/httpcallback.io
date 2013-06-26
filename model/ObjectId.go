8bb727b1871ee959845711448067e75e8e1e6956
Pieter Joost van de Sande <pj@born2code.net>
2013-06-26 08:24:16 +0200

    Adds tests for ObjectId

--- model/ObjectId.go
+++ model/ObjectId.go
@@ -1,6 +1,9 @@
 package model
 
 import (
+	"encoding/hex"
+	"errors"
+	"fmt"
 	"labix.org/v2/mgo/bson"
 )
 
@@ -15,4 +18,19 @@ func NewObjectId() ObjectId {
 
 func (id ObjectId) String() string {
 	return string(id)
+}
+
+func ParseObjectId(value string) (ObjectId, error) {
+	var id ObjectId
+	if len(value) != 24 {
+		return id, errors.New(fmt.Sprintf("Invalid object id. String lenght is %s while it must be %s", len(value), 24))
+	}
+
+	_, err := hex.DecodeString(value)
+	if err != nil {
+		return id, errors.New(fmt.Sprintf("Invalid object id. Not a valid hexidecimal string: %s", err.Error()))
+	}
+
+	id = ObjectId(bson.ObjectIdHex(value))
+	return id, nil
 }
