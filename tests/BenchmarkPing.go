af0a232e10e4fd59f96dcd113a4dffadf3fb0fb4
Pieter Joost van de Sande <pj@born2code.net>
2013-06-27 20:21:16 +0200

    Adds add user benchmark

--- tests/BenchmarkPing.go
+++ tests/BenchmarkPing.go
@@ -1,13 +1,38 @@
 package tests
 
 import (
+	"bytes"
+	"fmt"
 	. "launchpad.net/gocheck"
 	"net/http"
+	"time"
 )
 
 func (s *ApiIntegrationTestSuite) BenchmarkPing(c *C) {
 	for i := 0; i < c.N; i++ {
 		response, err := http.Get("http://api.localhost:8000/ping")
+
+		if err != nil {
+			c.Fatalf("Error while getting response: %v", err.Error())
+		} else {
+			response.Body.Close()
+		}
+	}
+}
+
+func (s *ApiIntegrationTestSuite) BenchmarkNewUser(c *C) {
+	t := time.Now().UnixNano()
+
+	for i := 0; i < c.N; i++ {
+		user := Document{
+			"username": fmt.Sprintf("pjvds%v", int64(i)+t),
+			"password": fmt.Sprintf("foobar%v", i),
+			"email":    fmt.Sprintf("pj%v@born2code.net", i),
+		}
+		data := user.ToJson()
+		buf := bytes.NewBuffer(data)
+
+		response, err := http.Post("http://api.localhost:8000/users", "application/json", buf)
 
 		if err != nil {
 			c.Fatalf("Error while getting response: %v", err.Error())
